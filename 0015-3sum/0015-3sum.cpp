a1aaa1a5c77a86e846ddb71d3b7347635674cef3
Gaurav Kumar <rajputgaurav9582@gmail.com>
2025-08-08 22:23:32 +0530

    Time: 44 ms (79.35%), Space: 29.1 MB (49.04%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,45 @@
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        vector<vector<int >> res;
+        for(int i = 0;i<n;i++) {
+            if(i >0 && nums[i] == nums[i- 1]) continue ;
             int j = i + 1;
-            int k = nums.size() - 1;
-
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.push_back({nums[i], nums[j], nums[k]});
+            int k = n  - 1;
+            while( j < k ) {
+                int sum = nums[i] + nums[j]  + nums[k];
+                if(sum < 0) {
                     j++;
 
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
                 }
+                else if(sum >0) {
+                    k--;
+
+                }
+                else {
+                    res.push_back({nums[i] , nums[j] , nums[k]});
+                    j++;
+                    while(  j<k && nums[j] == nums[j - 1])  j++;
+                    k--;
+                    while( j< k && nums[k] == nums[k+1]) k--;
+
+                }
+
             }
+          
         }
-        return res;        
+        return res ;
+        
     }
 };
+
+
+
+
+
+
+// here i use the 3 pointers in this how -> take o(n^2 ) for sure and i want to optiised o(n^3) to somewhere like o(n^2* logn ) ab=nd moreever i wnat to optimised the sc of this via not using extra ds to store unique triplets  ///
+
+//  this is best rQ for 2 pointer pattern -> most imp and rev  question before google intervIEW

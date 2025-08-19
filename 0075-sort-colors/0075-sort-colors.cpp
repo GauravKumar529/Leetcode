1941e1178d827931dc73eb7d0fc99281b0fe91ef
Gaurav Kumar <rajputgaurav9582@gmail.com>
2025-08-19 22:59:46 +0530

    Time: 2 ms (3.62%), Space: 11.6 MB (48.97%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-        //  just  imagine the diagram for this algo  . it is like (from 0 to low -1) all 0,s are present in the sorted  way and (from low  to mid-1 ) all 1,s in sorted order and (from mid to high all are in unsorted that is what we have to sort in such a way that it will this particular (mid to high ) will vanished away  and from (high =1) to n-1  all 2 ,s in sorted )  that a problem intution.
+        //  just  imagine the diagram for this algo  . it is like (from 0 to low -1) all 0,s are present in the sorted  way and (from low  to mid-1 ) all 1,s in sorted order and (from mid to high all are in unsorted that is what we have to sort in such a way that it will this particular (mid to high ) will VANISHED away  and from (high - 1) to n-1  all 2 ,s in sorted )  that a problem intution.
         int low  = 0; //keep the low , (mid or high {your choice }  pointer at ) first index 
         int high = n-1;// last index of nums[]
         int mid = 0;
@@ -11,19 +11,14 @@ public:
                 swap(nums[mid++],  nums[low++]);// we hve 3 cases if nums[mid] == 0,1,2   different  things possible can be visualize using diagram above 
             }
             else if(nums[mid] == 1){
-                mid++;// no changes becz its already sorted just move the pointer away
+                mid++;// no changes becz its already sorted ansd 1 aree sin sorted order  just move the pointer away
 
             }
-            else swap(nums[mid], nums[high--]);// keep the nums[mid] to the right ward in diagram 
+            else swap(nums[mid], nums[high--]);// keep the nums[mid] to the right ward in diagram  for 2 to be in sorted 
 
         }
         return ;//done
-
-
-
-
-
-        
-        
     }
+    
+
 };

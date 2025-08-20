67de32d607c13153bb53c956a05918c180919780
Gaurav Kumar <rajputgaurav9582@gmail.com>
2025-08-20 17:19:59 +0530

    Time: 8 ms (51.68%), Space: 128.3 MB (79.79%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,20 +1,21 @@
 class Solution {
-public:
+public: 
+#define vi vector<int> 
     vector<int> rearrangeArray(vector<int>& nums) {
-        int n  = nums.size();
-        int pi = 0;
-        int ni = 1;
-        vector<int> ans(n,0);
-        for(int i = 0;i<n;i++){
-            if(nums[i] > 0){
-                ans[pi] = nums[i];
-                pi+=2;
+        int n = nums.size();
+        int pos = 0;
+        int neg = 1;
+        vi ans(n , 0 );
+        for(int i  = 0;i<n;i++) {
+            if(nums[i]  > 0) {
+                ans[pos] = nums[i];
+                pos +=2;
             }
             else {
-                ans[ni] = nums[i];
-                ni+=2;
+                ans[neg ] = nums[i];
+                neg += 2;
             }
         }
-        return ans ;        
+        return ans ;   
     }
 };

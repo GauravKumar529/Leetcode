64501dcc13e7f68faeb63f8e4f78d0f90a3fbe69
Gaurav Kumar <rajputgaurav9582@gmail.com>
2025-08-24 23:05:45 +0530

    Time: 42 ms (88.51%), Space: 29 MB (89.68%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,45 +1,39 @@
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res; 
         int n = nums.size();
-        sort(nums.begin() , nums.end());
-        vector<vector<int >> res;
-        for(int i = 0;i<n;i++) {
-            if(i >0 && nums[i] == nums[i- 1]) continue ;
-            int j = i + 1;
-            int k = n  - 1;
-            while( j < k ) {
-                int sum = nums[i] + nums[j]  + nums[k];
+        sort(nums.begin(), nums.end());
+
+        int low = 0;
+        while(low < n - 2) {
+            if(low > 0 && nums[low] == nums[low-1]) { // skip duplicates for low
+                low++;
+                continue;
+            }
+
+            int mid = low + 1;
+            int high = n - 1;
+
+            while(mid < high) {
+                int sum = nums[low] + nums[mid] + nums[high];
                 if(sum < 0) {
-                    j++;
-
+                    mid++;
                 }
-                else if(sum >0) {
-                    k--;
-
+                else if(sum > 0) {
+                    high--;
                 }
                 else {
-                    res.push_back({nums[i] , nums[j] , nums[k]});
-                    j++;
-                    while(  j<k && nums[j] == nums[j - 1])  j++;
-                    k--;
-                    while( j< k && nums[k] == nums[k+1]) k--;
-
+                    res.push_back({nums[low], nums[mid], nums[high]});
+                    mid++;
+                    high--; 
+                    while(mid < high && nums[mid] == nums[mid-1]) mid++;
+                    while(mid < high && nums[high] == nums[high+1]) high--;
                 }
-
             }
-          
+            low++;
         }
-        return res ;
-        
+        return res;     
     }
 };
-
-
-
-
-
-
-// here i use the 3 pointers in this how -> take o(n^2 ) for sure and i want to optiised o(n^3) to somewhere like o(n^2* logn ) ab=nd moreever i wnat to optimised the sc of this via not using extra ds to store unique triplets  ///
-
-//  this is best rQ for 2 pointer pattern -> most imp and rev  question before google intervIEW

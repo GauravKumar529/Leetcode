c38d4d0ede2f324ec9227e37d808157130159f1e
Persistor <rajputgaurav9582@gmail.com>
2025-08-29 14:10:25 +0530

    Time: 0 ms (100%), Space: 9 MB (97.41%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int myAtoi(string s) {
+        int i = 0;
+        int sign = 1;
         int n = s.size();
-        if( n== 0) return 0;
-        int i = 0 , sign = 1;
+        if( n  == 0) return  0 ;
         long long ans = 0;
-        while(i< n && s[i] == ' ')  i++;
-        if(s[i] == '-') {
+
+        while(i< n && s[i] == ' ') i++;
+
+        if(s[i] == '-'&& i<n) {
             sign = -1;
             i++;
         }
-        else if(s[i] =='+' && i< n) i++;
-
-
-        while(i < n) {
+        else if(s[i] == '+' && i < n) i++;
+        while(i < n){
             if(s[i] >= '0' && s[i] <= '9') {
-                ans = ans* 10 + ( s[i] - '0');
-                if(sign == 1 &&  ans> INT_MAX) return INT_MAX;
-                if(sign == -1 && -ans < INT_MIN) return INT_MIN;
-                  i++;
+                ans = ans * 10 + (s[i] - '0');
+                if( sign == 1 && ans > INT_MAX ) return INT_MAX;
+                if(sign == -1 &&  -ans <  INT_MIN)  return INT_MIN;
+                i++;
             }
-            else break;
-        }return ans * sign;
-
-        
+            else break ; 
+        }
+        return ans* sign ;
     }
 };

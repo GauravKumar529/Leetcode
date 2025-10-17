82a76531d70b5dc6d4714da87c2d21a8b662333f
Persistor <rajputgaurav9582@gmail.com>
2025-10-17 13:27:41 +0530

    Time: 0 ms (100%), Space: 25.8 MB (93.42%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-
     int trap(vector<int>& height) {
         int n = height.size();
-        if(n ==0 ) return 0;
-        vector<int> preMax(n);
-        vector<int> suffixMax(n);
-        preMax[0] = height[0];
-        suffixMax[n-1] = height[n- 1];
-        //prefixMax  precmpute ;
-        for(int i = 1 ; i <  n;i++){
-            preMax[i] = max(height[i] , preMax[i- 1]);
+        int l= 0 , r = n-1 , lmax =0 , rmax = 0;
+        long long total = 0;
+        while(l< r){
+            if(height[l] <= height[r]){
+                if(lmax > height[l]) {
+                    total += lmax  - height[l];
+                }
+                else {
+                    lmax = height[l];
+                }
+                l++;
+            }
+            else {
+                if(rmax > height[r]) total += rmax - height[r];
+                else rmax = height[r];
+                r--;
+            }
         }
-
-        //suffixMax  precmpute ;
-        for(int i = n-2 ; i>=0;i--){
-            suffixMax[i] = max(height[i] , suffixMax[i+1]);
-        }
-
-
-        int total = 0;
-        long long trap = 0;
-        for(int i = 0 ;i< n;i++){
-            total = min(preMax[i] , suffixMax[i] ) - height[i];
-            if(total >0) trap += total ;
-
-            
-        }
-        return trap ;
-
+        return total ;
     }
 };

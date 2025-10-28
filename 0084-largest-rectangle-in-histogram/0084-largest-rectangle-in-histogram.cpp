c2a33ab51844c1fc606ca4c77c5715041d428f3b
Persistor <rajputgaurav9582@gmail.com>
2025-10-28 15:57:46 +0530

    Time: 17 ms (79.6%), Space: 81.4 MB (74.63%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-vector<int> getNSL(vector<int> & arr , int n ){
-    vector<int> res (n);
-    stack<int> st ;
-    for(int i = 0; i < n;i++) {
-        if(st.empty()) res[i] = -1;
-        else {
-            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-            res[i] = st.empty() ? -1: st.top();
-        }
-        st.push(i);
-
-    }return res;
-}
-vector<int> getNSR(vector<int> & arr , int n ){
-    vector<int> res (n);
-    stack<int> st ;
-    for(int i = n-1; i >=0;i--) {
-        if(st.empty()) res[i] = n;
-        else {
-            while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-            res[i] = st.empty() ? n: st.top();
-        }
-        st.push(i);
-
-    }return res;
-}
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int>NSL = getNSL(heights , n);
-        vector<int>NSR = getNSR(heights , n);
-        int area =0;
-
-        for(int i = 0; i < n;i++) {
-            int total = NSR[i] - NSL[i] - 1;
-            area = max(area , (int)total* heights[i]);
+        int maxArea =0;
+         int nse ,pse ;
+        stack<int> st;
+        for(int i =0 ; i< n;i++) {
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                int element = st.top();
+                st.pop();
+                nse = i ;
+                pse = st.empty() ?-1: st.top();
+                maxArea = max(maxArea , (int) (heights[element] * (nse - pse - 1))) ;
+            }
+            st.push(i) ;
         }
-        return area ;
+        while(!st.empty()){
+            int element = st.top();
+            st.pop();
+            nse = n ;
+            pse = st.empty()?-1 : st.top();
+            maxArea = max(maxArea , (int) (heights[element] * (nse - pse - 1))) ;
+        }
+        return maxArea ;
     }
 };

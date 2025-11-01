a76ee16ac503a909fc1fcf5aef2a018e116e7fe4
Persistor <rajputgaurav9582@gmail.com>
2025-11-02 02:49:21 +0530

    Time: 144 ms (20.53%), Space: 293.3 MB (5.05%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // Linked List → Array
+    vector<int> linkedListToArray(ListNode* head) {
+        vector<int> arr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            arr.push_back(curr->val);
+            curr = curr->next;
+        }
+        return arr;
+    }
+
+    // Array → Linked List
+    ListNode* arrayToLinkedList(const vector<int>& arr) {
+        if (arr.empty()) return nullptr;
+
+        ListNode* head = new ListNode(arr[0]);
+        ListNode* tail = head;
+
+        for (int i = 1; i < arr.size(); i++) {
+            tail->next = new ListNode(arr[i]);
+            tail = tail->next;
+        }
+        return head;
+    }
+
+    // Remove nums[] elements from linked list
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        vector<int> nums2 = linkedListToArray(head);
+        vector<int> res;
+
+        unordered_set<int> st(nums.begin(), nums.end());
+
+        for (int x : nums2) {
+            if (st.find(x) == st.end()) { // keep elements NOT in nums
+                res.push_back(x);
+            }
+        }
+
+        ListNode* NewHead = arrayToLinkedList(res);
+        return NewHead;
+    }
+};

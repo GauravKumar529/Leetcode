36d31d893eb9a18820726a76c7af2a18b972f792
Persistor <rajputgaurav9582@gmail.com>
2025-11-07 18:02:50 +0530

    Time: 35 ms (29.06%), Space: 139.2 MB (58.36%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // build intution-< after brute-> ask hm, kr kya rhe hai , kyon kar rhehai , ->>        what           d i need s for this one so that time comp less lge
+        // its not problem , its a concept 
+
+/*
+ story Points:
+    step1:when new element comes  nums[i], make space fot it ,(window size cannt then k)
+    step2: now , when nums[i]comes , there is no ned of elements lesser thrn nums[i]
+    step3: now push i in deque ->. for num[i];
+    step4:(i>= k-1) then deq.front() -> push in res   
+*/ 
+
         int n = nums.size();
+        deque<int> dq;
         vector<int> res ;
-        map<int ,int> mpp;
-        for(int i = 0; i< k;i++) mpp[nums[i]]++;
-        res.push_back(mpp.rbegin()->first);
-        for(int i = k ;i < n;i++) {
-            mpp[nums[i - k]]--;
-            if(mpp[nums[i - k]] ==0) mpp.erase(nums[i - k]);
-            mpp[nums[i]]++;
-            res.push_back(mpp.rbegin()->first);
+        for(int i = 0; i < n;i++) {
+            // step1
+            while(!dq.empty() && dq.front() <= (i-k)){
+                dq.pop_front();
+            }
+            // step2
+            while(!dq.empty() &&  nums[i] >= nums[dq.back()]){
+                dq.pop_back();
+            }
+            // step3
+            dq.push_back(i);
+            // step4
+            if(i>= k- 1) {
+                res.push_back(nums[dq.front()]);
+            }
         }
         return res ;
-        
+
+
     }
 };
